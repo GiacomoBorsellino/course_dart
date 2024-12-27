47634ebcf0619013e557537d5163c69cdd017713
GiacomoBorsellino <giacomo.borsellino@avrgroup.it>
2024-12-27 14:33:25 +0100

    setup

---
+++ code/bin/corso_dart.dart
@@ -0,0 +1,41 @@
+// import 'package:corso_dart/corso_dart.dart' as corso_dart;
+
+void main(List<String> arguments) {
+  // int intero;
+  // intero = 6;
+  // double decimale = 6.6;
+  // dynamic test = 9;
+  // test = true;
+  // print(intero);
+  // print(decimale);
+  // int error = 8;
+  // // error = 'aaa';
+  // print(error.runtimeType);
+  // test = "aaa";
+  // var test2;
+  // test2 = 5.5;
+  // test2 = "qwerty";
+  // print(test2.runtimeType);
+  // print('Hello world: ${corso_dart.calculate()}!');
+
+  // final prova = 1;
+  // prova = 2;
+
+  // final prova;
+  // prova = 2;
+
+  // const prova2 = 8;
+
+  final a = [1, 2, 3];
+  a.add(4);
+  print(a);
+
+  const b = [1, 2, 3];
+  b.add(4);
+  print(b);
+
+  int x = 6;
+  int y = 8;
+
+  print(x + y);
+}
