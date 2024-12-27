936c2504c918f837fd1bdf1fb9303ac39eb9edda
GiacomoBorsellino <giacomo.borsellino@avrgroup.it>
2024-12-27 17:22:11 +0100

    operations with types

--- code/bin/corso_dart.dart
+++ code/bin/corso_dart.dart
@@ -1,7 +1,45 @@
 // import 'package:corso_dart/corso_dart.dart' as corso_dart;
 
 void main(List<String> arguments) {
-  // int intero;
+  // num numero = 6.6;
+  // int intero = 0;
+  // double decimale = 1.1;
+
+  // print("ciao " + numero.toString());
+
+  // String stringa = '5';
+  // print(double.parse(stringa));
+
+  // double qwerty = 6.6;
+
+  // print(qwerty.ceil());
+  // print(qwerty.round());
+  // print(qwerty.floor());
+
+  // int numero = 10;
+  // int intero = 5;
+  // print(numero ~/ intero);
+
+  // int intero = 5;
+  // intero = intero + 5;
+  // intero += 5;
+  // print(intero);
+
+  // int intero = 5;
+  // // intero++;
+  // ++intero;
+  // print(intero);
+
+  int intero = 8;
+
+  int qw = intero++;
+
+  print(qw);
+
+  // print('Hello world: ${corso_dart.calculate()}!');
+}
+
+ // int intero;
   // intero = 6;
   // double decimale = 6.6;
   // dynamic test = 9;
@@ -16,7 +54,6 @@ void main(List<String> arguments) {
   // test2 = 5.5;
   // test2 = "qwerty";
   // print(test2.runtimeType);
-  // print('Hello world: ${corso_dart.calculate()}!');
 
   // final prova = 1;
   // prova = 2;
@@ -39,16 +76,15 @@ void main(List<String> arguments) {
 
   // print(x + y);
 
-  int? prova = 0;
-  double ops = 5.5;
-  num popa = 5.5;
-  String obs = 'aaa';
-  bool aba = true;
+  // int? prova = 0;
+  // double ops = 5.5;
+  // num popa = 5.5;
+  // String obs = 'aaa';
+  // bool aba = true;
 
-  List lista = [1, 2, 3];
-  Set sett = {1, 2, 3};
-  Map maps = {'nome': "Luca", 'cognome': "Rossi"};
+  // List lista = [1, 2, 3];
+  // Set sett = {1, 2, 3};
+  // Map maps = {'nome': "Luca", 'cognome': "Rossi"};
 
-  Runes runa = new Runes('\u{1f605}');
+  // Runes runa = new Runes('\u{1f605}');
   // print(new String.fromCharCodes(runa));
-}
